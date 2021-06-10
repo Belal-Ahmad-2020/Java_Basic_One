d7e4e87cc7a97b643abc0b9ce51aea68b39ca5ee
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-10 12:09:11 +0430

    Add files via upload

---
+++ Belal_Comparison_Operator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Comparison_Operator {
+    public static void main(String[] args) {
+        // Comparison Operators
+        int x, y;
+        
+        x=3;
+        y=2;
+        
+        if(x == 4) {
+            System.out.println("Hello");
+        }
+        else {
+            System.out.println("Bye");
+        }
+        
+        
+        if(y != 4) {
+            System.out.println("Hello Java");
+        }
+        
+        if(x > y) {
+            System.out.println("X is greater");
+        }
+        else {
+            System.out.println("Y is greater");
+        }
+        
+        
+        if(x >= y) {
+            System.out.println("X is greater or equal to y");
+        }
+        
+        
+        
+    }
+}
