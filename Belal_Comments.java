88886dc66eb80b901f28f9dc140faebe9c46032c
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-10 12:08:55 +0430

    Add files via upload

---
+++ Belal_Comments.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Comments {
+    // This is maing method 
+    public static void main(String[] args) {
+        // write a hello program 
+        System.out.println("Single Line Comments in java");
+        
+        /*
+        Java comments:
+            comments are used to describe our code.
+            it will help other programmers to know our code 
+            Comments can be single line, multi line or documentatioin comments
+        */
+        
+        String comments = "There are three types of comments in java";
+        System.out.println(comments);
+        
+        
+        /**
+       * Documentation Comments: are used to documents our code 
+       * for example 
+       * when I want to develop a software this type of comments will help  me to 
+       * document my software 
+         */
+        
+        
+        
+        
+    }
+}
