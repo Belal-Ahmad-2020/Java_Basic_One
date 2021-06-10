94c90779c7917e0d2a873cfce87d65ec14923289
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-10 12:12:02 +0430

    Add files via upload

---
+++ Belal_Data_Types.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Data_Types {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    public static void main(String[] args) {
+        /*
+        Primitive data types: specifies the size and type of a variable.
+            byte, short, int, long, float, double, boolean, char
+        
+        Non-Primitive data types: or reference type :  is used to refer to an object 
+            String, Array, Classes
+        
+        
+        */
+        
+        byte byteNum = 100;
+        short shortNum = 7000;
+        int intNum = 74000;
+        long longNum = 74000000;
+        float floatNum = 5.4f;
+        double doubleNum = 4.5d;
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        boolean isJavaEasy = true;
+        char character = 'a';
+        
+        String hello = "Hello World!";
+        int[] arrayNum = {10, 20, 30, 40};
+        
+        
+        System.out.println(hello);
+        System.out.println(character);
+                              
+        
+        
+    }
+}
+
+
