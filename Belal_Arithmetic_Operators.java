69d5ea1f30d6315835c5eea922c06d3d786c9187
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-10 12:07:10 +0430

    Add files via upload

---
+++ Belal_Arithmetic_Operators.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Arithmetic_Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        int num1, num2;
+        float result;
+        // Addition 
+        num1 = 4;
+        num2 = 3;
+        result = 4+3;
+        System.out.println(result);
+        
+        //Subtraction 
+        num1 = 4;
+        num2 = 3;
+        result = 4-3;
+        
+        // Multiplication
+        num1 = 4;
+        num2 = 3;
+        result = 4*3;
+        System.out.println(result);
+        
+           // Division
+        num1 = 4;
+        num2 = 3;
+        result =  (float) (4/3);
+        System.out.println(result);
+        
+           // Modulus
+        num1 = 4;
+        num2 = 3;
+        result = 4%3;
+        System.out.println(result);
+        
+        
+        // Increment and Decrement        
+        num1++;
+        num2--;
+        System.out.println(num1);
+        
+        
+        System.out.println(num2);
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
