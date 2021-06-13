27a59cade9cdad35a29eaac78075d9955a93ce16
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-13 08:38:56 +0430

    Add files via upload

---
+++ Belal_Find_Max_Min.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Find_Max_Min {
+    public static void main(String[] args) {
+        int x, y;
+        
+        x=5;
+        y=6;
+        
+        // Find The maximum number using max() method
+        System.out.println(Math.max(x, y));
+        
+        // FInd the minumum number using the min () method
+        System.out.println(Math.min(x, y));
+        
+    }
+}
