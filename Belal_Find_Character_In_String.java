0a6359d180c7aecb8e50294917d6261a472ce442
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-13 08:38:25 +0430

    Add files via upload

---
+++ Belal_Find_Character_In_String.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Find_Character_In_String {
+    public static void main(String[] main) {
+        // to find a spesifix character we can use indexOf() method
+        String msg = "Welcome to the AUAF!";
+        System.out.println(msg.indexOf("AUAF"));
+        
+        
+        String text = "Hello Computer Science";
+        System.out.println(msg.indexOf("Computer"));
+        
+        
+        
+    }
+}
