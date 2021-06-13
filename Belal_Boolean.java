a5dc073050375a50802f93c836a72eae6d48e12b
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-13 08:36:04 +0430

    Add files via upload

---
+++ Belal_Boolean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Boolean {
+    public static void main(String[] args) {
+        // in programming, you will need a data type that can have only two values 
+        
+        boolean is_electricity_problem;
+        is_electricity_problem = true;
+        System.out.println(is_electricity_problem);
+        
+        is_electricity_problem = false;
+        System.out.println(is_electricity_problem);
+        
+        // expression 
+        int x, y;        
+        x=5;
+        y=8;
+        
+        // check if x is greater than y
+        System.out.println(x>y);
+        
+        // check if x is less than y
+        System.out.println(x<y);
+        
+        // check if x is equal to y
+        System.out.println(x == y);
+        
+        
+        
+    }
+}
