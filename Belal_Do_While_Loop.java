5c941140cea09f7400edeaa651391f4fe205194e
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-13 08:37:44 +0430

    Add files via upload

---
+++ Belal_Do_While_Loop.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Do_While_Loop {
+    public static void main(String[] args) {
+        // do while loop
+        // this loop runs alwats once if the condition is not true
+        
+        int x = 100;
+        
+        do {
+            System.out.println(x);
+            x++;
+        }while( x < 100 );
+    }
+}
