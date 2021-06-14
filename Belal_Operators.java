4049594c844f3364719c2a4a26035499e4ab30ee
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-14 08:28:32 +0430

    Add files via upload

---
+++ Belal_Operators.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Operators {
+    public static void main(String[] args) {
+        /*
+        Operators: are used to perform operations
+        there are five types of operators in java:
+        Arithmetic (+, -, *, /, %, ++, --)
+        Assignment (=, +=, -=, *=, /=, &=, |=, ^=, >>=, <<= )
+        Comparison (==, !=, >, <, >=, <=)
+        Logical (&&, ||, !)
+        */
+        
+        
+        
+        
+    }
+}
