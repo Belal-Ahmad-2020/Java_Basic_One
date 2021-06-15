75e769e98245e70c6d66579733410077933f1ad0
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-15 11:45:03 +0430

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        variables are used to store data.
+        there are different types of variables in java
+        String  -- is used to store or text values
+        int     -- is used to storedecimal numbers 
+        float   -- is used to store floating point numbers
+        char    -- is used to store characters
+        boolean -- is used to store boolean values
+        */
+        
+        String name = "Ahmad";
+        int number = 22;
+        float num = 22.5f;
+        char letter = 'a';
+        boolean is_married = false;
+        
+        // declaring many variables at a time 
+        int a=3, b=5, c=9;
+        
+        
+        
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(num);
+        System.out.println(letter);
+        System.out.println(is_married);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+        
+        
+    }
+}
