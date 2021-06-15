d9dcc5ce792697b4e43db997a355e5b38f453d57
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-15 11:30:25 +0430

    Add files via upload

---
+++ Belal_Shorthand_Conditional_Statement.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Shorthand_Conditional_Statement {
+    public static void main(String[] args) {
+        // this is best practise to write if-else statements in shorthand
+    int x, y;
+    String msg;
+    
+    x = 5;
+    y = 7;
+    
+    msg = (x > y) ? "Hello" : "Bye";
+    
+    System.out.println(msg);
+    
+        
+    }
+}
