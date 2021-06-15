8dec75c9472591b19ec85a1b250cc388796ded38
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-15 11:43:11 +0430

    Add files via upload

---
+++ Belal_Working_With_Special_Characters.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3schools;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Working_With_Special_Characters {
+    public static void main(String[] args) {
+        // String must written within the double quotes 
+        // java will misunderstand this string, and genrate an error while using special characters
+        
+        String special;
+        special = "Hello \"Java\" ";
+        special = "Use only \'special characters\' inside the string";
+        special = "Display the text and go to the next line \n";
+        special = "Use tab to make \tab spaces";
+        special = "while using \b back slash";
+        
+        
+        
+    }
+}
