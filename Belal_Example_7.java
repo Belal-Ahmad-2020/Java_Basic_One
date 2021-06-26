dc3a703d4961657f812660ce1f489f00811aa2e5
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-26 08:36:32 +0430

    Add files via upload

---
+++ Belal_Example_7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_7 {
+    public static void main(String[] args) {
+        // add two variable
+        String fname = "Ahmad ";
+        String lname = "Ahmadi";
+        String fullName = fname + lname;
+        System.out.println(fullName);  
+  }
+}
