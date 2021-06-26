b2061c35f0c34d29918a01bca28e2ea54dd7a7f5
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-26 08:36:49 +0430

    Add files via upload

---
+++ Belal_Example_8.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_8 {
+  public static void main(String[] args) {
+      // declare many variables
+    int a = 2, b = 3, c = 5;
+    System.out.println(a + b + c);
+  }    
+}
