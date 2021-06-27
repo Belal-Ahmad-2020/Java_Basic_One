a815e766488b2a5432a1b8a3c8bc6d982b7d1b11
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-27 09:38:32 +0430

    Add files via upload

---
+++ Belal_Example_29.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_29 {
+  public static void main(String[] args) {
+      // Assignmetn Operator
+    int x = 5;
+    int y = 3;
+    System.out.println(x == y); // returns false because 5 is not equal to 3
+  }    
+}
