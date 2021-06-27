52b6d95a1ddfb588a597e1327efde0df0c2945e2
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-27 09:27:37 +0430

    Add files via upload

---
+++ Belal_Example_24.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_24 {
+     public static void main(String[] args) {
+         // Division Operator
+        int x = 12;
+        int y = 3;
+        System.out.println(x / y);
+  }
+}
