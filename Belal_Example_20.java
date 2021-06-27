b0606b38a82d4d51e0413fb9d2ee141b2081ca04
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-27 09:21:35 +0430

    Add files via upload

---
+++ Belal_Example_20.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_20 {
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; // Narrowing casting
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }    
+}
