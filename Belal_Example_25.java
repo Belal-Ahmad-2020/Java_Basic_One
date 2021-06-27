40352e34259e408b951d556de2564726018ca5bd
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-27 09:34:46 +0430

    Add files via upload

---
+++ Belal_Example_25.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_25 {
+  public static void main(String[] args) {
+      // Modulus Operator
+    int x = 5;
+    int y = 2;
+    System.out.println(x % y);
+  }    
+}
