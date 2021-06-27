00abfb087185acc629afaf501976effb5ffde787
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-27 09:21:02 +0430

    Add files via upload

---
+++ Belal_Example_19.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_19 {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // widenning casting 
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }    
+}
