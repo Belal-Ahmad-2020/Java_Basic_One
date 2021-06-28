099a1bb7c9c8b7d2883da48be35b0d237eebcb09
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-28 09:09:46 +0430

    Add files via upload

---
+++ Belal_Example_36.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_36 {
+  public static void main(String[] args) {
+      // finding a string inside another string 
+    String msg = "Hello Programming";
+    System.out.println(msg.indexOf("Programming"));
+  }    
+}
