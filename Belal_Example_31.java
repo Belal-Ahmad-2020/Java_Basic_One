278c83bc34c55a7647dd3b2d3e709246cf71970e
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-28 09:06:11 +0430

    Add files via upload

---
+++ Belal_Example_31.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_31 {
+  public static void main(String[] args) {
+      // Logical Operator
+    int x = 5;
+    System.out.println(x > 3 && x < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
+  }    
+}
