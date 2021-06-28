94e1ed76a31fc6a6211db2da5eec59e02082444c
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-28 09:09:05 +0430

    Add files via upload

---
+++ Belal_Example_35.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_35 {
+  public static void main(String[] args) {
+      // uppercase and lowercase methods 
+    String txt = "Hello World";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }    
+}
