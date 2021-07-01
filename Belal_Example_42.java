f6e071689eee35d040d49ebab16ea8ca573078f8
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-01 23:57:24 +0430

    Add files via upload

---
+++ Belal_Example_42.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_42 {
+  public static void main(String[] args) {
+      // find the absoulte positive number
+    System.out.println(Math.abs(-6.4));  
+  }    
+}
