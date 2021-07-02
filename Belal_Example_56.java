49aa0108000fb771e968930d2ac77c0192e4f534
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-02 22:22:07 +0430

    Add files via upload

---
+++ Belal_Example_56.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_56 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+          if (i == 5) {
+            break;
+          }
+          System.out.println(i);
+        }  
+  }
+}
