dca31b33ad13f90b8751f071eb3fa8001a515104
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-02 22:18:28 +0430

    Add files via upload

---
+++ Belal_Example_51.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_51 {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+          case 6:
+            System.out.println("Today is Saturday");
+            break;
+          case 7:
+            System.out.println("Today is Sunday");
+            break;
+          default:
+            System.out.println("Looking forward to the Weekend");
+        }    
+  }
+}
