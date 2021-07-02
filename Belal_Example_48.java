b94c4c373e52b17f274ae64f5f54a948092833f6
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-02 22:14:12 +0430

    Add files via upload

---
+++ Belal_Example_48.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_48 {
+    public static void main(String[] args) {
+        int time = 12;
+        if (time < 18) {
+          System.out.println("Good day.");
+        } else {
+          System.out.println("Good evening.");
+        }  
+  }
+}
