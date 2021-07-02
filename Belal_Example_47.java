51839d025a0e6b71388347f831baf5f4dd02cb98
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-02 22:13:20 +0430

    Add files via upload

---
+++ Belal_Example_47.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_47 {
+    public static void main(String[] args) {
+        // is statement 
+        if (30 > 28) {
+          System.out.println("30 is greater than 28");
+        }  
+  }
+}
