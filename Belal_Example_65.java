b151de05ca0bc719a874ade1f3ee47f28f90876d
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:02:20 +0430

    Add files via upload

---
+++ Belal_Example_65.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_65 {    
+  public static void main(String[] args) {
+    int[][] numbers = { {1, 2, 3, 4}, {5, 6, 7} };
+    int x = numbers[1][3];
+    System.out.println(x);  
+  }
+}
