46f68747eababbaf13adc7a5a9888bcb0892c324
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:14:30 +0430

    Add files via upload

---
+++ Belal_Example_94.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public abstract class Belal_Example_94 {
+    // abstract class with abstract methods
+    public abstract void eat();
+    public abstract void drink();
+    public abstract void code();
+    
+}
+
+
+
