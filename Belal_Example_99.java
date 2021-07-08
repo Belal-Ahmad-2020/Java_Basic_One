38edf518b5f80f6df3b1b66a0966805925b54e57
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:16:10 +0430

    Add files via upload

---
+++ Belal_Example_99.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+// import all packages 
+import java.util.*;
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_99 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("ENter a number: ");
+        int num = in.nextInt();
+        
+        System.out.println("Your enterd " + num);
+    }
+}
