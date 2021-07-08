1a730df06708f17e940993bda34f65a704997e05
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:03:08 +0430

    Add files via upload

---
+++ Belal_Example_66.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_66 {
+    public static void main(String[] args) {
+        
+        int[][] num = { {1, 2, 3, 4}, {5, 6, 7} };
+        
+        for (int i = 0; i < num.length; ++i) {
+            
+           for(int j = 0; j < num[i].length; ++j) {
+              System.out.println(num[i][j]);
+           }           
+           
+        }
+   }
+}
