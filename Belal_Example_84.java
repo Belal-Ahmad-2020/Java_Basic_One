cb57244c64710f0a83a9f75bbdca46d402f669d9
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:11:26 +0430

    Add files via upload

---
+++ Belal_Example_84.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_84 {
+    public static void main(String[] args) {
+   
+        K k1 = new K();
+        k1.speed(100);
+        
+  }
+}
+
+
+class K {
+    public void speed(int speed) {
+        System.out.println("Speed is " + speed);
+    }
+}
