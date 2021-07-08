bad0a3792f205ae7e0183db02a60f90f49974f01
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:13:16 +0430

    Add files via upload

---
+++ Belal_Example_90.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_90 {
+    public static void main(String[] args) {
+        P p1 = new P();
+        // age is private property , not accessible
+//        p1.age = 10;
+
+        
+    }
+}
+
+class P {
+    // public attributes
+    public String name, email;
+    // Private attributes
+    private int age;
+    
+    
+    
+    P() {
+        
+    }
+    
+    
+}
