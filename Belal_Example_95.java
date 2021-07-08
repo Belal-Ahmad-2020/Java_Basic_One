fb1568329025dca509de9b6313687ecf3ace43b7
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:14:53 +0430

    Add files via upload

---
+++ Belal_Example_95.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+class Belal_Example_95 extends Belal_Example_94 {
+    // extends from Belal_Example_94
+    // override all methods 
+    
+    @Override 
+    public void eat() {
+        System.out.println("eating");
+    }
+    
+    @Override 
+    public void drink() {
+        System.out.println("Drinking");
+    }
+    
+    @Override 
+    public void code() {
+        System.out.println("Coding");
+    }
+}
