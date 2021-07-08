6607d69b34135e3d666f11b62cde86cdc217f11c
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-07-08 08:08:05 +0430

    Add files via upload

---
+++ Belal_Example_74.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_Example_74 {
+    public static void main(String[] args) {
+        
+        System.out.println(addition(5, 5));
+        System.out.println(addDouble(4.5, 5.7));
+    }
+    
+    public static int addition(int x, int y) {
+    return x + y;
+  }
+  
+    public static double addDouble(double x, double y) {
+    return x + y;
+  }
+            
+}
